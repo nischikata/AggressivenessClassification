26f40348e025f648b637afc9c48d9bd1da9540b9
Nischikata <nischikata@gmail.com>
2017-05-13 18:40:44 +0200

    Lasso Selections done

---
+++ Utils/lasso_selections.py
@@ -0,0 +1,46 @@
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LassoCV
+import numpy as np
+
+
+def get_LassoSelections(dataset, out='SELECTIONS_lasso.pickle', step=0.005, maxThresh=0.25, minThresh=0.000005):
+    X = dataset["data"]
+    y = dataset["target"]
+    
+    thr_range =  np.arange(0.000005, 0.25, 0.005)
+    no_selection = np.arange(0,68)
+    
+    selections = [no_selection]
+    
+    for thresh in thr_range:
+        
+        clf = LassoCV()
+        sfm = SelectFromModel(clf, threshold=thresh)
+        sfm.fit(X,y)
+        sel = sfm.get_support(indices=True)
+        n_features = sfm.transform(X).shape[1]
+        
+        if len(sel) > 0 and len(sel) != len(selections[-1]):
+            selections.append(sel)
+          
+    selections.reverse()
+    save(selections, out)
+    return selections
+
+
+"""
+    # USAGE
+from setupDataset import get_dataset, save, load
+
+m_devSet = get_dataset("M_DEV_dataset.pickle")
+w_devSet = get_dataset("W_DEV_dataset.pickle")
+
+get_LassoSelections(m_devSet, out='M_SELECTIONS_lasso.pickle')
+get_LassoSelections(w_devSet, out='W_SELECTIONS_lasso.pickle')
+
+m_lasso_selections = load('M_SELECTIONS_lasso.pickle')
+w_lasso_selections = load('W_SELECTIONS_lasso.pickle')
+
+print m_lasso_selections, "\n\n"
+print w_lasso_selections
+"""
